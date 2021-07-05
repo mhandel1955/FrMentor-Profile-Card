c0ec6abc8c87119a0409c8c063a49b691b9c1086
mhandel1955 <mhandel1955@gmail.com>
2021-07-05 15:25:12 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppBkgrnd></AppBkgrnd>
+</template>
+
+<script>
+import AppBkgrnd from "./components/AppBkgrnd.vue";
+export default {
+  name: "App",
+  components: {
+    AppBkgrnd,
+  },
+};
+</script>
+
+<style>
+#app {
+  margin: 0;
+}
+</style>
---
+++ src/components/AppBkgrnd.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="app-frame">
+    <svg
+      id="circle01"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="slice"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="a">
+          <stop stop-color="#0989B4" stop-opacity="0" offset="0%" />
+          <stop stop-color="#53FFEE" offset="99.94%" />
+        </linearGradient>
+      </defs>
+
+      <ellipse
+        fill="url(#a)"
+        transform="rotate(-135 50 50)"
+        cx="50"
+        cy="50"
+        rx="50"
+        ry="50"
+        fill-rule="evenodd"
+        opacity="1"
+      />
+    </svg>
+    <svg
+      id="circle02"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="slice"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="a">
+          <stop stop-color="#0989B4" stop-opacity="0" offset="0%" />
+          <stop stop-color="#53FFEE" offset="99.94%" />
+        </linearGradient>
+      </defs>
+
+      <ellipse
+        fill="url(#a)"
+        transform="rotate(45 50 50)"
+        cx="50"
+        cy="50"
+        rx="50"
+        ry="50"
+        fill-rule="evenodd"
+        opacity="1"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBkgrnd.vue",
+  components: {},
+};
+</script>
+
+<style>
+.app-frame {
+  background: rgb(27, 160, 173);
+  height: 100vh;
+  width: 100vw;
+}
+#circle01 {
+  height: 100vh;
+  width: 100vh;
+  position: fixed;
+  top: -45vh;
+  left: -45vh;
+}
+#circle02 {
+  height: 100vh;
+  width: 100vh;
+  position: fixed;
+  bottom: -45vh;
+  right: -45vh;
+}
+</style>
